b5632f917216556c102d35ca470aa0ba01ad8cbd
Revazashvili <levanirevazashvili4@gmail.com>
2020-12-24 13:13:20 +0400

    asd

---
+++ Client/Consumer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Net.RabbitMQ.Models.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Client
+{
+    public class Consumer : BackgroundService
+    {
+        private readonly IConsumer _consumer;
+        private readonly ILogger<Consumer> _logger;
+        public Consumer(IConsumer consumer,ILogger<Consumer> logger)
+        {
+            _consumer = consumer;
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _consumer.Subscribe<Test>(MessageProccessor);
+            return Task.CompletedTask;
+        }
+        
+        private Task MessageProccessor(Test test)
+        {
+            Console.WriteLine($"Id : {test.Id} Name : {test.Name}");
+            return Task.CompletedTask;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"consuming at {DateTime.Now}");
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Net.RabbitMQ.Extensions;
+using Net.RabbitMQ.Models.ValueObjects;
+
+namespace Client
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile(@"C:\Projects\NetCore\Net.RabbitMQ\Client\appsettings.json", true);
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                        config.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    //var config = hostContext.Configuration.GetSection("RabbitMqConfiguraion").Get<RabbitMQConfiguration>();
+                    var config = new RabbitMQConfiguration()
+                    {
+                        RabbitMqConnection = new RabbitMqConnectionConfig
+                        {
+                            HostName = "localhost",
+                            VirtualHost = "/",
+                            Port = 5672,
+                            UserName = "guest",
+                            Password = "guest"
+                        },
+                        Exchange = new Exchange
+                        {
+                            Name = "testexchange",
+                            Type = "fanout"
+                        },
+                        DLExchange = new Exchange
+                        {
+                            Name = "dltestexchange",
+                            Type = "fanout"
+                        },
+                        Queue = new Queue
+                        {
+                            Name = "testqueue",
+                            Durable = true,
+                            AutoDelete = false,
+                            Exclusive = false
+                        },
+                        DLQueue = new Queue
+                        {
+                            Name = "dltestqueue",
+                            Durable = true,
+                            AutoDelete = false,
+                            Exclusive = false
+                        },
+                        PrefetchCount = 10,
+                        PrefetchSize = 0,
+                        Routing = "test"
+                    };
+                    services.AddProducer(config);
+                    services.AddSubscriber(config);
+                    services.AddHostedService<Publisher>();
+                    services.AddHostedService<Consumer>();
+                });
+            await builder.RunConsoleAsync();
+        }
+    }
+}
